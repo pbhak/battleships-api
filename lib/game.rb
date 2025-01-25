832d1a4512c3fe562755e64fa976b8baa992cda9
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-01-24 18:55:02 -0800

    implemented place function

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,71 @@
 
 # Class representing the core game
 class Game
-  SHIP_LENGTHS = [5].freeze
+  attr_reader :ships, :board
+
+  SHIPS = {
+    carrier: 5,
+    battleship: 4,
+    destroyer: 3,
+    submarine: 3,
+    patrol_boat: 2
+  }.freeze
 
   def initialize
     # Creates a new two-dimensional array representing a 10x10 game board
     @board = Array.new(10) { Array.new(10, :empty) }
+    @ships = create_ships
   end
 
-  # def place_ships
+  def place_ship(start_location, end_location) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+    current_ship = nil
+    @ships.each_value do |ship|
+      if ship[:location].empty?
+        current_ship = ship
+        break
+      end
+    end
+
+    return false if current_ship.nil?
+    return false unless valid_position?(start_location, end_location, current_ship[:size])
+
+    # Both location parameters MUST start with a letter for this to work properly
+    start_col = start_location[0].ord - 96 # f5 -> 6 (f is column 6)
+    end_col = end_location[0].ord - 96
+
+    start_row = start_location[1].to_i # f5 -> 5 (row 5)
+    end_row = end_location[1].to_i
+
+    is_vertical = start_col == end_col # true if ship is to be placed vertically
+
+    if is_vertical
+      # column stays the same
+      until start_row == end_row + 1
+        occupy(start_col, start_row)
+        @ships.each do |ship_name, ship|
+          next unless ship == current_ship
+
+          @ships[ship_name][:location] << [start_col, start_row]
+          break
+        end
+        start_row += 1
+      end
+    else
+      # row stays the same
+      until start_col == end_col + 1
+        occupy(start_col, start_row)
+        @ships.each do |ship_name, ship|
+          next unless ship == current_ship
+
+          @ships[ship_name][:location] << [start_col, start_row]
+          break
+        end
+        start_col += 1
+      end
+    end
+
+    true
+  end
 
   def to_s
     @board.each_with_index do |row, index|
@@ -18,6 +75,48 @@ class Game
   end
 
   # Helper functions
+  def valid_position?(start_location, end_location, size) # rubocop:disable Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/AbcSize,Metrics/PerceivedComplexity
+    start_location = start_location.downcase
+    end_location = end_location.downcase
+
+    # Both location parameters MUST start with a letter for this to work properly
+    start_col = start_location[0].ord - 96 # f5 -> 6 (f is column 6)
+    end_col = end_location[0].ord - 96
+
+    start_row = start_location[1].to_i # f5 -> 5 (row 5)
+    end_row = end_location[1].to_i
+
+    is_vertical = start_col == end_col
+
+    # Start and end locations must actually exist
+    unless start_col.between?(1, 10) && end_col.between?(1, 10) && start_row.between?(1, 10) && end_row.between?(1, 10)
+      return false
+    end
+
+    # Start and end location must be on the same column or the same row - ships cannot be placed diagonally
+    return false unless start_col == end_col || start_row == end_row
+
+    # Distance between start and end location must match the size of the ship being placed
+    return false unless [(end_row - start_row + 1), (end_col - start_col + 1)].include?(size)
+
+    # All space between start and end must be empty
+    if is_vertical
+      until start_row == end_row + 1
+        return false unless empty?(start_col, start_row)
+
+        start_row += 1
+      end
+    else
+      until start_col == end_col + 1
+        return false unless empty?(start_col, start_row)
+
+        start_col += 1
+      end
+    end
+
+    true
+  end
+
   def empty?(col, row)
     @board[row - 1][col - 1] == :empty
   end
@@ -42,11 +141,20 @@ class Game
     [rand(1..10), rand(1..10)]
   end
 
+  def create_ships
+    ships = {}
+    SHIPS.each do |ship_name, ship_size|
+      ships[ship_name] = {
+        size: ship_size,
+        sunk: false,
+        location: []
+      }
+    end
+    ships
+  end
+
   # TODO: create segment to add or randomize ship positions on board
   # TODO: create turn loop and win/lose conditions
 end
 
 x = Game.new
-
-x.place_ships
-puts x
